417c890a50d21631d17ac0fb4bd3b1a0d99f9e56
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-05-21 23:43:09 -0400

    home links in text

--- front-end/src/components/Home/Home.jsx
+++ front-end/src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Home.css'
 
 const Home = () => {
@@ -7,9 +8,9 @@ const Home = () => {
       <h1>Welcome to the Movie Database</h1>
       <p><em>This is the home page of the Movie Database application.<br />Use the navigation menu to explore different features.</em></p>
       <ul>
-        <li><b>Search Movies</b><br /><em>Search for movies and add them to your collection.</em></li>
-        <li><b>My Movies</b><br /><em>View and manage your personal movie collection.</em></li>
-        <li><b>My Watchlists</b><br /><em>Create and manage your movie watchlists.</em></li>
+        <li><Link className='link' to="/search"><b>Search Movies</b></Link><br /><em>Search for movies and add them to your collection.</em></li>
+        <li><Link className='link' to="/my-movies"><b>My Movies</b></Link><br /><em>View and manage your personal movie collection.</em></li>
+        <li><Link className='link' to="/watchlists"><b>My Watchlists</b></Link><br /><em>Create and manage your movie watchlists.</em></li>
       </ul>
     </div>
   );
